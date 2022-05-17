efbbc847a26af554a88ff3b7c1e12493347dc7a4
Naachiket Pant <naachiketpant88@gmail.com>
2022-05-17 22:49:18 +0530

    Add green and blue

--- include/colors.h
+++ include/colors.h
@@ -7,5 +7,7 @@ GLfloat COLOR_WHITE[] = {1.0, 1.0, 1.0};
 GLfloat COLOR_CYAN[] = {0.2, 1.0, 1.0};
 GLfloat COLOR_BLACK[] = {0.0, 0.0, 0.0};
 GLfloat COLOR_RED[] = {1.0, 0.0, 0.0};
+GLfloat COLOR_GREEN[] = {0.0, 1.0, 0.0};
+GLfloat COLOR_BLUE[] = {0.0, 0.0, 1.0};
 
 #endif
